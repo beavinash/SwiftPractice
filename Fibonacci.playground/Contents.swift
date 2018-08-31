40f1697b882dc9e9fa77d0fc033642e72a814001
Avinash Reddy <beavinash15@gmail.com>
2018-08-30 19:30:05 -0700

    code update.

---
+++ Fibonacci.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+func fibonacci(input: Int) -> [Int] {
+    var result = [0, 1]
+    for i in 2..<input {
+        let a = result[i - 1]
+        let b = result[i - 2]
+        result.append(a + b)
+    }
+    return result
+}
+
+fibonacci(input: 10)
--- PrimeNumber.playground/Contents.swift
+++ PrimeNumber.playground/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
+func primeNumber1(number: Int) -> Bool {
+    
+    guard number > 1 else {
+        return false
+    }
+    for num in 2..<number {
+        if number % num == 0 {
+            return false
+        }
+    }
+    return true
+}
 
+primeNumber1(number: 100)
+primeNumber1(number: 3)
