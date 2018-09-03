c7d036339acf977400aaf43baaf42d3331325def
beavinash <beavinash15@gmail.com>
2018-09-03 10:49:34 -0700

    testing this

--- Fibonacci.playground/Contents.swift
+++ Fibonacci.playground/Contents.swift
@@ -10,4 +10,15 @@ func fibonacci(input: Int) -> [Int] {
     return result
 }
 
+func fibonacci1(input: Int) -> [Int] {
+    var result = [0, 1]
+    for i in 2..<input {
+        let a = result[i - 1]
+        let b = result[i - 2]
+        result.append(a + b)
+    }
+    return result
+}
+
 fibonacci(input: 10)
+fibonacci1(input: 5)
